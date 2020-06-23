8ddc6d7054bbcb98349a16993e020f70d99c36da
Nicholas Egnatz <npegnatz@gmail.com>
2020-06-23 14:20:03 -0400

    1.5

--- js/index.js
+++ js/index.js
@@ -2,6 +2,10 @@ $(document).on("click", "#dashboard-button", function () {
 	window.location.href = "home.html";
 });
 
+$(document).on("click", "#login-button", function () {
+	var appkey = "b260700071156fd889a144a3b5f0"
+	window.location.href = "home.html";
+});
 
 $(document).on("click", "#logout-button", function () {
 	window.open("https://c6ezh197.caspio.com/folderlogout");
--- js/nav.js
+++ js/nav.js
@@ -5,6 +5,5 @@ $(document).on('click', '.navbar-burger', function () {
 
 
 $(document).on("click", "#logout-button", function () {
-	window.open("https://c6ezh197.caspio.com/folderlogout");
-	window.location.href = "/index.html";
+	window.location.href = "https://c6ezh197.caspio.com/folderlogout";
 });
